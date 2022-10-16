c4d7a9dd691206b62d7f3fafd1a3660651196f2c
arafat0007 <yasinarafat0007@gmail.com>
2022-10-16 20:07:14 +0900

    Initial project files

---
+++ Reactive-Mongo-CRUD_Backend/src/test/java/com/example/ReactiveMongoCRUD/ReactiveMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveMongoCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
